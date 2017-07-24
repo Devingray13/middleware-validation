3a9d4fb14431eb9275e841629e4f3f71e9ee9f39
Devin Gray <devin.gray@colorado.edu>
2017-07-24 15:03:40 -0600

    working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var regex = /^\d+$/;
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,10 +11,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next){
+router.post('/', tooOld, isNumber,  checkString, function(req, res, next){
   console.log("posted")
   console.log(req.body)
-  res.render('index', {title: 'Posted'})
+  res.render('index', {title: "You're in!"})
 })
+
+function isNumber(req, res, next){
+  if (typeof + req.body.age !== 'number'){
+    res.render('index', {title: 'try again'})
+  }
+  else {next()}
+
+}
+
+
+
+function checkString(req, res, next){
+  if ( !regex.test(req.body.age)) {
+    console.log(req.body);
+    res.render('index', {title:"enter number"})
+  }
+
+  else {next()}
+
+}
+
+
+
+function tooOld(req, res, next){
+  if (+req.body.age > 100){
+    res.render('index', {title:'Too old!'})
+  }
+  else {next()}
+}
 
 module.exports = router;
